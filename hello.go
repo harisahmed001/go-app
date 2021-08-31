c3165c964347a40782e5fe3f7f5f74c89a831be4
Waleed Ahmed <waleed.ahmed@cloudways.com>
2021-08-31 07:07:57 +0500

    test

--- hello.go
+++ hello.go
@@ -14,6 +14,6 @@ func main() {
 }
 
 func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
-   fmt.Fprintf(w, "Hello world v4!")
+   fmt.Fprintf(w, "Hello world v5!")
    w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
 }
